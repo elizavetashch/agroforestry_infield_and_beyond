08eb7cc0120d84b1a02d95e4ed84c3d4492abad9
elizavetashch <shcherbinina.elisaveta@gmail.com>
2026-09-21 03:41:39 +0200

    three simple models

--- 02_scripts/02_exploration_modelling/08_finalmodel_night.R
+++ 02_scripts/02_exploration_modelling/08_finalmodel_night.R
@@ -1,271 +1,116 @@
 
 
-library(terra)
-# plots for the presentation 
-
-rfile <- "C:/Users/Elizaveta/OneDrive - Universität Bayreuth/Dokumente/MasterThesis/MA_RProject/01_Data/AnalysisData/LULC/Wendhausen_field_2021_buf3000m.tif"
-r <- terra::rast(rfile)
-plot(r)
+# preparation -------------------------------------------------------------
 
 
-m_swf <- gam(
-  yield_rel ~
-    s(distance_to_tree_strip, by =  photo_path, k = 5) +
-    swf_slope_0to200 +
-    swf_slope_200to500 +
-    swf_slope_500to1000 +
-    ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
-    ti(distance_to_tree_strip, treeage, k = c(3, 3)) +
-    AFage +
-    PC1_c +
-    PC1_s +
-    l_shdi +
-    l_ed +
-    s(field, bs = "re") +
-    s(year, bs = "re"),
-  data = mod_data,
-  family = gaussian,
-  method = "REML"
+library(refund)   
+library(mgcv)    
+library(tidyverse)
+library(gratia) 
+
+mod_data <- read.csv("01_Data/20260920_moddata.csv")
+swf_mat <- read.csv("01_Data/20260920_swf_mat.csv")
+
+mod_data$year <- as.factor(mod_data$year)
+mod_data$field <- as.factor(mod_data$field)
+mod_data$photo_path <- as.factor(mod_data$photo_path)
+
+radii <- c(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
+
+swf_mat <- as.matrix(mod_data[, paste0("swf_d", radii)])
+
+mod_data$swf_slope_0to200 <- apply(
+  swf_mat[, radii >= 100 & radii <= 200, drop = FALSE],
+  1,
+  function(y) coef(lm(y ~ radii[radii >= 100 & radii <= 200]))[2]
 )
 
-summary(m_swf)
+mod_data$swf_slope_200to500 <- apply(
+  swf_mat[, radii >= 200 & radii <= 500, drop = FALSE],
+  1,
+  function(y) coef(lm(y ~ radii[radii >= 200 & radii <= 500]))[2]
+)
 
-# 6.1 Basis Dimension Adequacy
-# Rule: k-index < 1 AND p < 0.05 → basis too restrictive; increase k in s().
-k.check(m_swf)
+mod_data$swf_slope_500to1000 <- apply(
+  swf_mat[, radii >= 500 & radii <= 1000, drop = FALSE],
+  1,
+  function(y) coef(lm(y ~ radii[radii >= 500 & radii <= 1000]))[2]
+)
 
-# 6.2 Residual Diagnostics
-# Plot 	Good Sign
-# QQ Plot 	Points on the diagonal
-# Residuals vs Fitted 	Random scatter around zero
-# Histogram 	Bell-shaped, symmetric
-# Response vs Fitted 	Points on the 1:1 line
 
+summary(mod_data)
+
+
+# easiest model -----------------------------------------------------------
+
+m_0 <- gam(yield_rel ~ s(distance_to_tree_strip, k = 5) + s(year, bs = "re"),
+    data = mod_data,
+    family = gaussian,
+    method = "REML"
+)
+
+summary(m_0)
+k.check(m_0)
 par(mfrow = c(2, 2))
-gam.check(m_swf, pch = 16, cex = 0.5)
+gam.check(m_0, pch = 16, cex = 0.5)
 par(mfrow = c(1, 1))
-
-# 6.3 Concurvity
-# Rule: Values near 1 → one smooth approximated by others → unstable estimates. Values < 0.8 are acceptable.
-concurvity(m_swf, full = TRUE)
-
-plot(m_swf, shade = TRUE, shade.col = "lightblue",
+plot(m_0, shade = TRUE, shade.col = "lightblue",
      seWithMean = TRUE, scale = 0, residuals = TRUE,
      pch = 16, cex = 0.3, col = "grey50")
-AIC(m_swf)
 
-# marginal curve
-p  <- predict(m_swf2, newdata=mod_data, type="response", se.fit=TRUE)
-mod_data$fit   <- p$fit
-mod_data$lower <- p$fit - 1.96*p$se.fit
-mod_data$upper <- p$fit + 1.96*p$se.fit
+AIC(m_0)
+# interaction model -----------------------------------------------------------
 
-ggplot(mod_data, aes(distance_to_tree_strip, fit)) +
-  geom_ribbon(aes(ymin=lower, ymax=upper), alpha=0.2, fill="steelblue") +
-  geom_line(color="steelblue", linewidth=1.2) +
-  geom_rug(data=df, aes(x=distance_to_tree_strip), inherit.aes=FALSE, alpha=0.15) +
-  labs(title="Marginal Effect: Temperature on Species Abundance",
-       subtitle="Rainfall & Elevation at medians | 95% CI shaded",
-       x="Temperature (°C)", y="Predicted Abundance") +
-  theme_minimal(base_size=12)
-
-# summaries
-s <- summary(m_swf2)
-print(round(s$s.table, 3))
-
-# model swf without field random effect  ----------------------------------
-
-m_swf2 <- gam(
-  yield_rel ~
-    s(distance_to_tree_strip, by =  crop_unified, k = 5) +
-    swf_slope_0to200 +
-    swf_slope_200to500 +
-    swf_slope_500to1000 +
-    ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
-    ti(distance_to_tree_strip, treeage, k = c(3, 3)) +
-    AFage +
-    PC1_c +
-    PC1_s +
-    l_shdi +
-    l_ed +
-    s(year, bs = "re"),
-  data = mod_data,
-  family = gaussian,
-  method = "REML"
+m_1 <- gam(yield_rel ~ s(distance_to_tree_strip, k = 5) + 
+             swf_slope_0to200 +
+             swf_slope_200to500 + 
+             swf_slope_500to1000 + 
+             ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
+             s(year, bs = "re"),
+    data = mod_data,
+    family = gaussian,
+    method = "REML"
 )
 
-AIC(m_swf2)
-s <- summary(m_swf2)
-k.check(m_swf2)
-
+summary(m_1)
+k.check(m_1)
 par(mfrow = c(2, 2))
-gam.check(m_swf2, pch = 16, cex = 0.5)
+gam.check(m_1, pch = 16, cex = 0.5)
 par(mfrow = c(1, 1))
+plot(m_1, shade = TRUE, shade.col = "lightblue",
+     seWithMean = TRUE, scale = 0, residuals = TRUE,
+     pch = 16, cex = 0.3, col = "grey50")
 
-as.table(concurvity(m_swf2, full = TRUE))
+AIC(m_1)
+# interaction model with field characteristics -----------------------------------------------------------
 
-
-# mod 4  ------------------------------------------------------------------
-
-
-m_swf4 <- gam(
-  yield_rel ~
-    s(distance_to_tree_strip, by =  photo_path, k = 5) +
-    
-    prop_swf_within +
-    swf_slope_0to200:prop_swf_within + 
-    
-    swf_slope_200to500 +
-    swf_slope_500to1000 +
-    ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
-    ti(distance_to_tree_strip, treeage, k = c(3, 3)) +
-    AFage +
-    PC1_c +
-    PC1_s +
-    l_shdi +
-    l_ed +
-    s(year, bs = "re"),
-  data = mod_data,
-  family = gaussian,
-  method = "REML"
+m_2 <- gam(yield_rel ~ s(distance_to_tree_strip, k = 5) + 
+             swf_slope_0to200 +
+             swf_slope_200to500 + 
+             swf_slope_500to1000 + 
+             ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
+             ti(distance_to_tree_strip, treeage, k = c(3, 3)) +
+             treeage + 
+             AFage +
+             PC1_c +
+             PC1_s +
+             l_shdi +
+             l_ed +
+             s(year, bs = "re"),
+    data = mod_data,
+    family = gaussian,
+    method = "REML"
 )
 
-AIC(m_swf4)
-summary(m_swf4)
-k.check(m_swf4)
-
+summary(m_2)
+k.check(m_2)
 par(mfrow = c(2, 2))
-gam.check(m_swf4, pch = 16, cex = 0.5)
+gam.check(m_2, pch = 16, cex = 0.5)
 par(mfrow = c(1, 1))
+plot(m_2, shade = TRUE, shade.col = "lightblue",
+     seWithMean = TRUE, scale = 0, residuals = TRUE,
+     pch = 16, cex = 0.3, col = "grey50")
 
-as.table(concurvity(m_swf4, full = TRUE))
-
-
-
-# model3 with swf einzeln -------------------------------------------------
-
-m_swf3 <- gam(
-  yield_rel ~
-    s(distance_to_tree_strip, by =  photo_path, k = 5) +
-    swf_d100 +
-    swf_d200 +
-    swf_d300 +
-    swf_d400 +
-    swf_d500 +
-    swf_d600 +
-    swf_d700 +
-    swf_d800 +
-    swf_d900 +
-    swf_d1000 +
-    ti(distance_to_tree_strip, treeage, k = c(3, 3)) +
-    AFage +
-    PC1_c +
-    PC1_s +
-    l_shdi +
-    l_ed +
-    s(year, bs = "re"),
-  data = mod_data,
-  family = gaussian,
-  method = "REML"
-)
-
-AIC(m_swf3)
-
-k.check(m_swf3)
-
-par(mfrow = c(2, 2))
-gam.check(m_swf3, pch = 16, cex = 0.5)
-par(mfrow = c(1, 1))
-
-as.table(concurvity(m_swf3, full = TRUE))
-
-summary(m_swf3)
+AIC(m_2)
 
 
-
-# distance to relative yield  ---------------------------------------------
-
-ggplot(
-  mod_data,
-  aes(
-    x = distance_to_tree_strip,
-    y = yield_rel
-  )
-) +
-  geom_point(alpha = 0.4) +
-  geom_smooth(
-    method = "gam",
-    formula = y ~ s(x, k = 5),
-    se = TRUE
-  ) +
-  labs(
-    x = "Distance to tree strip (m)",
-    y = "Relative yield",
-    colour = "Photo path"
-  ) +
-  theme_minimal(base_size = 14)
-
-
-
-
-
-# model pfr ---------------------------------------------------------------
-
-
-m_pfr <- pfr(
-  yield_tha ~
-    lf(swf_mat, argvals = swf_argvals, k = 5) +
-    s(distance_to_tree_strip, by = photo_path, k = 5) +
-    ti(distance_to_tree_strip, treeage, k = c(5, 5)) +
-    AFage +
-    PC1_c +
-    PC1_s +
-    l_shdi +
-    l_ed +
-    s(year, bs = "re"),
-  data = mod_data,
-  method = "REML"
-)
-
-par(mfrow = c(2, 2))
-gam.check(m_pfr)
-summary(m_pfr)
-
-AIC(m_pfr_int, m_pfr)
-
-
-
-# mod5 final --------------------------------------------------------------
-
-m_swf4_revised <- gam(
-  yield_rel ~
-    s(distance_to_tree_strip, photo_path, bs = "fs", k = 5) +
-    swf_slope_0to200 + 
-    swf_slope_200to500 +
-    swf_slope_500to1000 +
-    ti(distance_to_tree_strip, swf_slope_0to200, k = c(5, 5)) +
-    prop_swf_within +
-    swf_slope_0to200:prop_swf_within +   
-    ti(distance_to_tree_strip, treeage, k = c(5, 4)) +
-    treeage + AFage + 
-    PC1_c + PC1_s +
-    l_shdi +
-    l_ed +
-    s(year, bs = "re"),
-  data = mod_data,
-  family = gaussian,
-  method = "REML"
-)
-
-AIC(m_swf4_revised)
-  
-
-AIC(m_swf2)
-s <- summary(m_swf4_revised)
-k.check(m_swf4_revised)
-
-par(mfrow = c(2, 2))
-gam.check(m_swf4_revised, pch = 16, cex = 0.5)
-par(mfrow = c(1, 1))
-
-as.table(concurvity(m_swf4_revised, full = TRUE))
